daca5795cab246d2b4cbbf489829651dcad423c3
kamalsayed <44102251+kamalsayed@users.noreply.github.com>
2018-12-15 03:32:19 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -55,34 +55,12 @@ class GameBoard(tk.Frame):
             self.canvas.tag_lower("square")
 
 
-
-def take_input():
-    """Accepts the size of the chess board"""
-
-    while True:
-        try:
-            size = int(input('What is the size of the chessboard? n = \n'))
-            if size == 1:
-                print("Trivial solution, choose a board size of atleast 4")
-            if size <= 3:
-                print("Enter a value such that size>=4")
-                continue
-            return size
-        except ValueError:
-            print("Invalid value entered. Enter again")
-
-
-
 if __name__ == "__main__":
-    def get_board(size):
-        """Returns an n by n board"""
-        board = [0] * size
-        for ix in range(size):
-            board[ix] = [0] * size
-        return board
 
-    def is_safe(board, row, col, size):
-        """Check if it's safe to place a queen at board[x][y]"""
+
+    #each time get a new column to check the safety of it's left side of the row and -              -
+    def is_safe(board, row, col, size):                                             #  -          -
+        """Check if it's safe to place a queen at board[x][y]"""                       # -  &&  -
 
         # check row on left side
         for iy in range(col):
@@ -125,92 +103,13 @@ if __name__ == "__main__":
                 # backtrack
                 board[i][col] = 0
 
-
     def add_solution(board):
         """Saves the board state to the global variable 'solutions'"""
         global solutions
         saved_board = copy.deepcopy(board)
         solutions.append(saved_board)
 
-
-    def take_input():
-        """Accepts the size of the chess board"""
-
-        while True:
-            try:
-                size = int(input('What is the size of the chessboard? n = \n'))
-                if size == 1:
-                    print("Trivial solution, choose a board size of atleast 4")
-                if size <= 3:
-                    print("Enter a value such that size>=4")
-                    continue
-                return size
-            except ValueError:
-                print("Invalid value entered. Enter again")
-
-
-    def get_board(size):
-        """Returns an n by n board"""
-        board = [0] * size
-        for ix in range(size):
-            board[ix] = [0] * size
-        return board
-
-
-    def print_solutions(solutions, size):
-        """Prints all the solutions in user friendly way"""
-        for sol in solutions:
-            for row in sol:
-                print(row)
-
-            return
-
-
-    def is_safe(board, row, col, size):
-        """Check if it's safe to place a queen at board[x][y]"""
-
-        # check row on left side
-        for iy in range(col):
-            if board[row][iy] == 1:
-                return False
-
-        ix, iy = row, col
-        while ix >= 0 and iy >= 0:
-            if board[ix][iy] == 1:
-                return False
-            ix -= 1
-            iy -= 1
-
-        jx, jy = row, col
-        while jx < size and jy >= 0:
-            if board[jx][jy] == 1:
-                return False
-            jx += 1
-            jy -= 1
-
-        return True
-
-
-    def solve(board, col, size):
-        """Use backtracking to find all solutions"""
-        # base case
-        if col >= size:
-            return
-
-        for i in range(size):
-            if is_safe(board, i, col, size):
-                board[i][col] = 1
-                if col == size - 1:
-                    add_solution(board)
-                    board[i][col] = 0
-                    return
-                solve(board, col + 1, size)
-                # backtrack
-                board[i][col] = 0
-
-
-
-
+    #smile icon code
     imagedata = '''
             R0lGODlhEAAQAOeSAKx7Fqx8F61/G62CILCJKriIHM+HALKNMNCIANKKANOMALuRK7WOVLWPV9eR
             ANiSANuXAN2ZAN6aAN+bAOCcAOKeANCjKOShANKnK+imAOyrAN6qSNaxPfCwAOKyJOKyJvKyANW0
@@ -232,20 +131,26 @@ if __name__ == "__main__":
             BQMEFPigAsoRBQM1BGLjRIiOGSxWBCmToCCMOXSW2HCBo8qWDQcvMMkzCNCbHQga/qMgAYIDBQZU
             yxYYEAA7
         '''
-
     size = 8
-    board = get_board(size)
+    board = [[0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0]] #to build a matrix 8*8 of 0's
     solutions = []
     solve(board, 0, size)
     s = size
     root = tk.Tk()
     b= GameBoard(root,rows=s,columns=s)
     b.pack(side="top", fill="both", expand="true", padx=4, pady=4)
-    player1 = tk.PhotoImage(data=imagedata)
+    Queen = tk.PhotoImage(data=imagedata)
     for _ in range(size):
         for x in range(size):
-            if solutions[0][_][x] == 1:
-                b.addpiece("player"+str(x), player1,_, x)
+            if solutions[1][_][x] == 1:
+                b.addpiece("Queen"+str(x),Queen ,_, x)
 
     print("Total solutions = {}".format(len(solutions)))
 
